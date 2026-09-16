796e868a1535be1ec51050584fe48e836a1ada9f
Ronhenley08121 <falsarioaaron@gmail.com>
2026-09-16 22:29:43 +0800

    Deployment fixing 2,0

--- vite.config.js
+++ vite.config.js
@@ -10,24 +10,15 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
 
-        adminDashboard: resolve(__dirname, 'Assets/Admin_dashboard/Admin.html'),
+        adminDashboard: resolve(__dirname, 'Assets/Admin_dashboard/adminDashboard.html'),
         adminPassword: resolve(__dirname, 'Assets/Admin_dashboard/password/password.html'),
-        adminPenalties: resolve(__dirname, 'Assets/Admin_dashboard/penalties/student.html'),
-        adminReports: resolve(__dirname, 'Assets/Admin_dashboard/report/report.html'),
-        adminSettings: resolve(__dirname, 'Assets/Admin_dashboard/settings/setting.html'),
-        adminStudents: resolve(__dirname, 'Assets/Admin_dashboard/students/record.html'),
-        adminAppeals: resolve(__dirname, 'Assets/Admin_dashboard/appeal/appeal.html'),
 
         studentAuth: resolve(__dirname, 'Assets/Student_Authentication/Student.html'),
 
         adminLogin: resolve(__dirname, 'Assets/Student_Authentication/Admin_Authentication/Admin.html'),
         adminUpdatePass: resolve(__dirname, 'Assets/Student_Authentication/Admin_Authentication/update-password.html'),
 
-        studentDashboard: resolve(__dirname, 'Assets/Student_Dashboard/stud.html'),
-        studentAppeal: resolve(__dirname, 'Assets/Student_Dashboard/appeal/appeal.html'),
-        studentHistory: resolve(__dirname, 'Assets/Student_Dashboard/history/history.html'),
-        studentPenalties: resolve(__dirname, 'Assets/Student_Dashboard/penalties/penalties.html'),
-        studentSettings: resolve(__dirname, 'Assets/Student_Dashboard/settings/setting.html'),
+        studentDashboard: resolve(__dirname, 'Assets/Student_Dashboard/studentDashboard.html'),
       }
     }
   },
